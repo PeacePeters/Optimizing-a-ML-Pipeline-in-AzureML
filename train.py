e762199e41f80bf6f1203b9d4721c68e8821f87d
PeacePeters <68206315+PeacePeters@users.noreply.github.com>
2020-11-17 09:00:05 +0100

    Update train.py

--- train.py
+++ train.py
@@ -52,6 +52,7 @@ x, y = clean_data(ds)
 # Split data into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
     
+# Get the experiment run context
 run = Run.get_context()
 
 
