24fddedd873dc1108261d04894276c2374d2e1e8
PeacePeters <68206315+PeacePeters@users.noreply.github.com>
2020-11-23 02:08:00 +0100

    Add files via upload
    Final commit

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ def clean_data(data):
 x, y = clean_data(ds)
 
 # Split data into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
     
 # Get the experiment run context
 run = Run.get_context()
